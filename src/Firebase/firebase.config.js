79c0678ddefcf1a462df443248031bda0156b5fe
RabeyaEty01 <etyrabeya01@gmail.com>
2021-10-21 19:19:04 +0600

    google,github and facebook sign up added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCXsRyWRGgMT7A7oznMDvtpK31bHLTFqrs",
+    authDomain: "simple-firebase-auth-d21e1.firebaseapp.com",
+    projectId: "simple-firebase-auth-d21e1",
+    storageBucket: "simple-firebase-auth-d21e1.appspot.com",
+    messagingSenderId: "32705737124",
+    appId: "1:32705737124:web:895d3da44a491fbb61d131"
+  };
+
+  export default firebaseConfig;
